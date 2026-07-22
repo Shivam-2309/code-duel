60184468d391a506991272db1eca254a8d289db2
Shivam Kapoor <kapoorshivam7178@gmail.com>
2026-07-22 19:02:46 +0530

    Initial Setup + NeonDB setup done

---
+++ prisma/migrations/20260722125927_init/migration.sql
@@ -0,0 +1,25 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cfHandle" TEXT,
+    "verified" BOOLEAN NOT NULL DEFAULT false,
+    "rating" INTEGER NOT NULL DEFAULT 0,
+    "wins" INTEGER NOT NULL DEFAULT 0,
+    "losses" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_cfHandle_key" ON "users"("cfHandle");
