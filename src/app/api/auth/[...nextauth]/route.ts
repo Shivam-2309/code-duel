0812149a05d7f2aef3e9f362dfcd2b46f8894ac3
Shivam Kapoor <kapoorshivam7178@gmail.com>
2026-07-23 15:01:44 +0530

    Feat: Auth Setup done

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,3 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import { prisma } from "@/lib/prisma";
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    Credentials({
-      credentials: {
-        email: { type: "email" },
-        password: { type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) return null;
-
-        const email = credentials.email as string;
-        const password = credentials.password as string;
-
-        const user = await prisma.user.findUnique({ where: { email } });
-        if (!user?.password) return null;
-
-        const isValid = await bcrypt.compare(password, user.password);
-        if (!isValid) return null;
-
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.username,
-          verified: user.verified,
-        };
-      },
-    }),
-  ],
-});
-
+import { handlers } from "@/auth";
+// Basically yeh yahan pr isliye h taaki hr jagah await auth ki configuration to call same rhe
 export const { GET, POST } = handlers;
---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { auth } from "@/auth";
+import { NextAuthRequest } from "next-auth";
+import { NextResponse } from "next/server";
+
+export default auth((req: NextAuthRequest) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const publicRoutes = ["/login", "/register"];
+
+  const isAuthApi = nextUrl.pathname.startsWith("/api/auth");
+
+  if (isAuthApi) {
+    return NextResponse.next();
+  }
+
+  if (
+    isLoggedIn &&
+    (nextUrl.pathname === "/login" || nextUrl.pathname === "/register")
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
+
+  if (!isLoggedIn && !publicRoutes.includes(nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
+  console.log("NEXT-URL", nextUrl);
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\..*).*)"],
+};
